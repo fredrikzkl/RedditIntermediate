fe182c91b0f664766f533ab2fe1fd8f422e8b5e5
Fredrik Kloster <fredrik_kloster@hotmail.com>
2014-03-18 09:36:21 +0100

    Few Changes

--- telephoneKeypadsListner.java
+++ telephoneKeypadsListner.java
@@ -2,12 +2,15 @@ package telephoneKeypadsIntermediate;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class telephoneKeypadsListner implements ActionListener {
 	
+	
+	static ArrayList<String> indexes = new ArrayList<String>();
 	
 	
 	private JTextField inputbox;
@@ -20,20 +23,41 @@ public class telephoneKeypadsListner implements ActionListener {
 		
 	}
 	
+	
+	public static void CreateArray(String s){
+		
+		for(String substring: s.split(" ")){
+			
+			String a = "2485083572085748";
+			int count = 0;
+			for (int i = 0, len = a.length(); i < len; i++) {
+			    if (Character.isDigit(a.charAt(i))) {
+			        count++;
+			    }
+			}
+			
+			
+	        indexes.add(substring);
+	        }
+		
+		
+		
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
 		String input;
 		input = inputbox.getText();
-		String[] parts = input.split(" ");
-		for (String a:parts){
-			System.out.print(a);
-		}
+		
+		CreateArray(input);
 		
 		
 		
-		//int inputInt = Integer.parseInt(nigg);
+		
+		
+		
+		
 		
 		
 		
